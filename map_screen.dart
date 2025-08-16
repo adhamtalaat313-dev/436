f63c576f828370b1bafd95f5b34cfd58d024ec2e
adhamtalaat313-dev <adhamtalaat313@gmail.com>
2025-08-16 17:55:40 +0300

    Create map_screen.dart

---
+++ map_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'موقع الطفل على الخريطة',
+        style: TextStyle(fontSize: 22),
+      ),
+    );
+  }
+}
