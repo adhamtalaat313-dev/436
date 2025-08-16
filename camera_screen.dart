8fc7dd194024505df272f93cedc0452b072864a8
adhamtalaat313-dev <adhamtalaat313@gmail.com>
2025-08-16 17:55:16 +0300

    Create camera_screen.dart

---
+++ camera_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'شاشة كاميرا الطفل',
+        style: TextStyle(fontSize: 22),
+      ),
+    );
+  }
+}
